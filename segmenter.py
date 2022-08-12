c21b775a93059b19236a929146b81077abc3a262
Chris Dawson <chris@vivoh.com>
2022-08-12 13:41:04 -0400

    Restore code with side effects

--- segmenter.py
+++ segmenter.py
@@ -4,10 +4,10 @@ from new_reader import reader
 
 def _segmenter():
     print("Starting segmenter")
-    x9k3 = X9K3("udp://127.0.0.1:3000")
+    x9k3 = X9K3()
     # print(x9k3)
     x9k3.live = True
-    # x9k3._tsdata = reader()
+    x9k3._tsdata = reader("udp://127.0.0.1:3000")
     x9k3.output_dir = "files"
     x9k3.decode()
 
